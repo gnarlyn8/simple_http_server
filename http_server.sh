36240fd1540a12f381d3f836878ede7c4917a97c
Nathen Bowes <nbowes17@gmail.com>
2025-10-02 12:59:30 -0700

    add conditional to only allow GET requests

--- http_server.sh
+++ http_server.sh
@@ -4,8 +4,13 @@ function server () {
 
   while true
   do
-    read message
-    echo "You said: $message"
+    read method path version
+    if [[ $method -eq "GET"]]
+    then
+      echo "HTTP/1.1 200 OK"
+    else
+      echo "HTTP/1.1 400 Bad Request"
+    fi
   done
 }
 
