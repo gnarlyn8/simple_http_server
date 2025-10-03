cbade564a55b1bbc5213386fde0c98b7a078ada1
Nathen Bowes <nbowes17@gmail.com>
2025-10-02 17:43:28 -0700

    allow for multi-line GET requests from browser

--- http_server.sh
+++ http_server.sh
@@ -20,7 +20,20 @@
 function server () {
   while true
   do
-    read method path version
+    # read method path version
+    message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
     if [[ "$method" = 'GET' ]]
     then
       if [[ -f "./www/$path" ]]
